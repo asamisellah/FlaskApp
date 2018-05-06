d983d0eb306efd5a4b4953c8b15602fbd14463b7
asamisellah <asami.rachael@gmail.com>
2018-05-06 20:50:09 +0300

    feat[READ]: Implement a Read Feature -Create a task class -Create index method to get all tasks -Create get_task method to get a single task -Create templates for these methods

--- app.py
+++ app.py
@@ -1,12 +1,29 @@
-from flask import Flask
-
+from flask import Flask, redirect, url_for, render_template
+from task import Task
 
 app = Flask(__name__)
 
+first = Task('First Task', 'This is my first task')
+second = Task('Second Task', 'This is my second task')
+third = Task('Third Task', 'This is my third task')
+
+tasks = [first, second, third]
+
 @app.route('/')
 def root():
-    return 'Hello World'
+    return redirect(url_for('index'))
+
+@app.route('/tasks')
+def index():
+    return render_template('index.html', tasks = tasks)
+
+@app.route('/tasks/<int:id>', methods=["GET"])
+def get_task(id):
+    task = [task for task in tasks if task.id == id]
+    return render_template('show.html', task = task[0])
+
+
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
